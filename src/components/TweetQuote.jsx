a75805b6c2ad9f719c4a35dadadde8aacce19ea6
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-03-23 21:43:40 +0100

    Added inline style for background color

--- src/components/TweetQuote.jsx
+++ src/components/TweetQuote.jsx
@@ -1,5 +1,5 @@
-function TweetQuote() {
-	return <a id="tweet-quote" href="https://twitter.com/intent/tweet" target={"_blank"}>Tweet</a>
+function TweetQuote(props) {
+	return <a id="tweet-quote" style={{backgroundColor: props.color}} href="https://twitter.com/intent/tweet" target={"_blank"}>Tweet</a>
 }
 
 export default TweetQuote;
